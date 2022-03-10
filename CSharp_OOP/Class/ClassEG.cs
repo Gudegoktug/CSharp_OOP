07f721559ee26e034c07a38806f47ad0d313d3b0
Göktuğ Güde <goktu1927@hotmail.com>
2022-03-10 16:43:17 +0300

    Proje dosyası ekle.

---
+++ CSharp_OOP/Class/ClassEG.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_OOP.Class
+{
+    public partial class ClassEG : Form
+    {
+        public ClassEG()
+        {
+            InitializeComponent();
+        }
+
+        private void ClassEG_Load(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//if you wanna use the class you created. you have to define an instance.
+            //"ClassName + variableName + = + new + ClassName"
+            //E.G
+            PoisonSword psword = new PoisonSword();
+
+            //we defined an instance.Now you can assign value and read value.
+            psword.AttackSpeed = int.Parse(textBox2.Text);
+            psword.AttackValue = int.Parse(textBox1.Text);
+            psword.SkillDamage = int.Parse(textBox3.Text);
+
+            MessageBox.Show($"Attack Speed: {psword.AttackSpeed}\nAttack Value: {psword.AttackValue}\nSkillDamage: {psword.SkillDamage} ");
+
+        }
+    }
+}
---
+++ CSharp_OOP/Class/PoisonSword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP.Class
+{
+     class PoisonSword //default internal. you create public class you cant use members of the class in other class
+    {
+        //if you wanna use any field in your project(different class) you have to make it public!!!
+       public int AttackValue;
+       public int AttackSpeed;
+       public int SkillDamage;
+
+    }
+}
